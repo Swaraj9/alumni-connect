c950f7f9acb5f802cbaf56cceb0454202f62d3d7
Swaraj9 <swaraj.dusane90@gmail.com>
2023-03-22 15:00:08 +0530

    server changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import './App.css';
+import { AuthContext } from './context/AuthContext';
 import AdminDashboard from './pages/AdminDashboard';
 import AlumniDashboard from './pages/AlumniDashboard';
 import Login from './pages/Login';
@@ -7,6 +9,9 @@ import TeacherDashboard from './pages/TeacherDashboard';
 import UploadLOA from './pages/uploadloa';
 
 function App() {
+  const {user, isAuthenticated} = useContext(AuthContext)
+  console.log(user);
+  console.log(isAuthenticated);
   return (
     <div className="App">
         <UploadLOA />
