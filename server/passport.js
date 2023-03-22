8091a4aa1ee5b34c61bfc4727b42379b39c56d65
Swaraj9 <swaraj.dusane90@gmail.com>
2023-03-22 14:28:32 +0530

    server changes

--- server/passport.js
+++ server/passport.js
@@ -1,7 +1,41 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
 const User = require('./models/User');
 
-passport.use(new LocalStrategy((username, password, done)=>{
+const cookieExtractor = (req) => {
+    let token = null;
+    if(req && req.cookies){
+        token = req.cookies["access_token"];
+    }
+    return token;
+}
 
-}))
+//Authorization
+passport.use(new JwtStrategy({
+    jwtFromRequest : cookieExtractor,
+    secretOrKey : "AlumniConnect"
+}, (payload, done) => {
+    User.findById({_id : payload.sub}, (err, user) => {
+        if(err)
+            return done(err, false);
+        if(user)
+            return done(null, user);
+        else   
+            return done(null, false);
+    })
+}));
+
+//Authentication
+passport.use(new LocalStrategy((username, password, done)=>{
+    User.findOne({username}, (err, user) => {
+        //Something went wrong with the database
+        if(err)
+            return done(err);
+        //User doesn't exist
+        if(!user)
+            return done(null, false);
+        //User exists
+        user.comparePassword(password, done);
+    });
+}));
--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose')
 
-const User = require('./models/User');
-
 //Create Server
 const port = 5000;
 const app = express();
@@ -13,7 +11,6 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
-
 //MongoDB connection
 try{
     mongoose.connect("mongodb+srv://swaraj:Swaraj 90@cluster0.lliqhu4.mongodb.net/?retryWrites=true&w=majority");
@@ -21,16 +18,10 @@ try{
     console.error("Database error occured" + err);
 }
 
-mongoose.connection.on('connected', () => console.log("Database successfully connected"))
+mongoose.connection.on('connected', () => console.log("Database successfully connected"));
 
-const userInput = {
-    username : "Swaraj",
-    password : "1234567",
-    role : "admin"
-}
-
-const user = new User(userInput);
-user.save().then(doc => console.log(doc))
+const userRouter = require('./routes/User');
+app.use('/user', userRouter);
 
 //Start Server
 app.listen(port, () => {
