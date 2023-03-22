f6dfcbaca866aacf570c588ea7ed2966eb1e5d42
Tavion20 <tavionalric@gmail.com>
2023-03-22 17:10:54 +0530

    Bug Fixes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,4 @@
 import React, { useRef, useState, useEffect } from "react";
-import React from "react";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import Input from "../components/Input";
 import  logo from '../img/kjsieit-logo.svg';
