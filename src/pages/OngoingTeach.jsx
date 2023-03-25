48eaf96efa20071fcabce7f10f28414f11ff4471
Tavion20 <tavionalric@gmail.com>
2023-03-25 10:23:44 +0530

    bugs

--- src/pages/OngoingTeach.jsx
+++ src/pages/OngoingTeach.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Input from "../components/Input";
 import { TbHexagonNumber1,TbHexagonNumber2,TbHexagonNumber3 } from 'react-icons/tb';
 import somaiya from "../img/kjsieit-logo.svg"
@@ -36,6 +36,18 @@ const EventCard = ({ children }) => {
   };
 
 const OngoingTeach = () => {
+  const [events, setEvents] = useState([]);
+  useEffect(()=>{
+    fetch('/event/get', {
+      method: "GET",
+    })
+    .then(res => res.json())
+    .then(_events => {
+      console.log(_events);
+      setEvents(_events);
+    })
+    .catch(err => console.log(err));
+  },[])
   return (
     <div
           style={{
@@ -54,81 +66,32 @@ const OngoingTeach = () => {
                 Leaderboard
             </div> */}
 
-          <Card>
-            <div style={{  height:'100px', width:'100px', backgroundColor: 'var(--white)', borderRadius:'50rem', marginRight:'2rem', }}>
-              <TbHexagonNumber1 style={{width:100,height:100}} />
-            </div>
-            <div style={{ flex: 3 ,marginTop:"0.8rem", fontSize:20}}>
-              <div>EventName</div>
-              <div><p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum deleniti non corporis eveniet laudantium modi distinctio iure quis praesentium natus aperiam exercitationem consequatur eaque provident eos, doloremque facere eligendi vel?</p></div>
-              {/* <div>
-              <button
-                style={{
-                    minWidth: "200px",
-                    border: "none",
-                    backgroundColor: "var(--primary)",
-                    padding: "0.5rem",
-                    color: "var(--white)",
-                    borderRadius: "5px",
-                    fontSize: "1rem",
-                    marginRight:'1rem'
-                }}
-                >
-                I'm Interested
-            </button>
-              </div> */}
-            </div>
-          </Card>
-
-          <Card>
-          <div style={{  height:'100px', width:'100px', backgroundColor: 'var(--white)', borderRadius:'50rem', marginRight:'2rem', }}>
-              <TbHexagonNumber2 style={{width:100,height:100}} />
-            </div>            <div style={{ flex: 3 ,marginTop:"0.8rem", fontSize:20}}>
-              <div>EventName</div>
-              <div><p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum deleniti non corporis eveniet laudantium modi distinctio iure quis praesentium natus aperiam exercitationem consequatur eaque provident eos, doloremque facere eligendi vel?</p></div>
-              {/* <div>
-              <button
-                style={{
-                    minWidth: "200px",
-                    border: "none",
-                    backgroundColor: "var(--primary)",
-                    padding: "0.5rem",
-                    color: "var(--white)",
-                    borderRadius: "5px",
-                    fontSize: "1rem",
-                    marginRight:'1rem'
-                }}
-                >
-                I'm Interested
-            </button>
-              </div> */}
-            </div>
-          </Card>
-
-          <Card>
-          <div style={{  height:'100px', width:'100px', backgroundColor: 'var(--white)', borderRadius:'50rem', marginRight:'2rem', }}>
-              <TbHexagonNumber3 style={{width:100,height:100}} />
-            </div>            <div style={{ flex: 3 ,marginTop:"0.8rem", fontSize:20}}>
-              <div>EventName</div>
-              <div><p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum deleniti non corporis eveniet laudantium modi distinctio iure quis praesentium natus aperiam exercitationem consequatur eaque provident eos, doloremque facere eligendi vel?</p></div>
-              {/* <div>
-              <button
-                style={{
-                    minWidth: "200px",
-                    border: "none",
-                    backgroundColor: "var(--primary)",
-                    padding: "0.5rem",
-                    color: "var(--white)",
-                    borderRadius: "5px",
-                    fontSize: "1rem",
-                    marginRight:'1rem'
-                }}
-                >
-                I'm Interested
-            </button>
-              </div> */}
-            </div>
-          </Card>
+{events && 
+            events.map(event => 
+            <Card>
+              <div style={{  height:'100px', width:'100px', backgroundColor: 'var(--white)', borderRadius:'50rem', marginRight:'2rem', }}/>
+              <div style={{ flex: 3 ,marginTop:"0.8rem", fontSize:20}}>
+                <div>{event.name}</div>
+                <div><p >{event.description}</p></div>
+                <div>
+                <button
+                  style={{
+                      minWidth: "200px",
+                      border: "none",
+                      backgroundColor: "var(--primary)",
+                      padding: "0.5rem",
+                      color: "var(--white)",
+                      borderRadius: "5px",
+                      fontSize: "1rem",
+                      marginRight:'1rem'
+                  }}
+                  >
+                  I'm Interested
+              </button>
+                </div>
+              </div>
+            </Card>
+            )}
 
           <div style={{ marginBottom: "1.5rem", fontSize:'2.5rem', color:'#A02929', alignSelf:'flex-start', marginTop:"1.5rem" }}>
                 <b>Suggeted Events</b>
@@ -138,8 +101,8 @@ const OngoingTeach = () => {
           <div style={{  height:'100px', width:'100px', backgroundColor: 'var(--white)', borderRadius:'50rem', marginRight:'2rem', }}>
               <TbHexagonNumber1 style={{width:100,height:100}} />
             </div>            <div style={{ flex: 3 ,marginTop:"0.8rem", fontSize:20}}>
-              <div>EventName</div>
-              <div><p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum deleniti non corporis eveniet laudantium modi distinctio iure quis praesentium natus aperiam exercitationem consequatur eaque provident eos, doloremque facere eligendi vel?</p></div>
+              <div>AzureCloud</div>
+              <div><p >Learn Cloud Computing  -- Workshop</p></div>
               <div>
               <button
                 style={{
